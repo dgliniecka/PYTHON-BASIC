f22caa86cc1dfe5a8ce9185ce7194b9e0133f2d3
Diana Gliniecka <107244877+dgliniecka@users.noreply.github.com>
2022-10-14 11:40:26 +0200

    Update task1.py

--- practice/1_python_part_1/task1.py
+++ practice/1_python_part_1/task1.py
@@ -11,8 +11,18 @@ Examples:
     >>> delete_from_list([], 'b')
     []
 """
-from typing import List, Any
-
-
-def delete_from_list(list_to_clean: List, item_to_delete: Any) -> List:
-    ...
+    def delete_from_list(list_to_clean, item_to_delete):
+    if item_to_delete in list_to_clean:
+        print('Initial list:', list_to_clean)
+        print('Item to delete:', item_to_delete)
+        findindex = list_to_clean.index(item_to_delete)
+        list_to_clean.pop(findindex)
+        print("List after deletion:",list_to_clean)
+    else: 
+        print('Item does not exist in the initial list', list_to_clean)
+        
+delete_from_list(['A', 'B', 'C'], 'D')
+delete_from_list([1, 2, 3, 4, 3], 3)
+delete_from_list(['a', 'b', 'c', 'b', 'd'], 'b')
+delete_from_list([1, 2, 3], 'b')
+delete_from_list([], 'b')
