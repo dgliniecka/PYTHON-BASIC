13d3a63ec75b7c5c65e9fce92749d20066895218
Diana Gliniecka <107244877+dgliniecka@users.noreply.github.com>
2022-10-17 11:11:59 +0200

    Update task2.py

--- practice/1_python_part_1/task2.py
+++ practice/1_python_part_1/task2.py
@@ -11,6 +11,27 @@ Examples:
 """
 from typing import Dict
 
-
 def set_to_dict(dict_to_update: Dict[str, int], **items_to_set) -> Dict:
-    ...
+    print("Dictionary before: ", dict_to_update)
+    print("Items to set: ", items_to_set)
+    if dict_to_update == {} and items_to_set != {}:
+      dict_to_update = items_to_set
+      print("Updated dictionary: ", dict_to_update)
+      
+      return dict_to_update
+    elif dict_to_update != {} and items_to_set == {}:
+      print("Updated dictionary: ", dict_to_update)
+      return dict_to_update
+    elif dict_to_update == {} and items_to_set == {}:
+      print('Both values empty!')
+      return dict_to_update
+    else:
+      for i in items_to_set.keys():
+        if dict_to_update[i]<items_to_set[i]:
+          dict_to_update[i]=items_to_set[i]
+          print("Updated dictionary: ", dict_to_update)
+          return dict_to_update
+       
+set_to_dict({'a': 1, 'b': 2, 'c': 3}, a=0, b=4)
+set_to_dict({}, a=0)
+set_to_dict({'a': 5})
