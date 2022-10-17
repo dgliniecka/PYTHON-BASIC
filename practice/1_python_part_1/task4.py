f0a23b1dcbbb36a840dd4aeea65f639e97472fae
Diana Gliniecka <107244877+dgliniecka@users.noreply.github.com>
2022-10-17 13:16:52 +0200

    Update task4.py

--- practice/1_python_part_1/task4.py
+++ practice/1_python_part_1/task4.py
@@ -7,7 +7,16 @@ Examples:
     [1, 4, 7]  # because [1^2, 2^2 - (1^2 - 1), 3^2 - (2^2 - 2)]
 """
 from typing import List
+def calculate_power_with_difference(ints: List[int], power: int) -> List[int]:
+  new = ints.copy()
+  new[0] = new[0] ** power
+  length = len(ints)
+  i = 1
+  while i < length:
+    new[i] = new[i] ** power
+    new[i] = new[i] - (new[i-1] - ints[i-1])
+    i += 1
+  print(new)
+  return new
 
-
-def calculate_power_with_difference(ints: List[int]) -> List[int]:
-    ...
+calculate_power_with_difference([1, 2, 3], 2)
