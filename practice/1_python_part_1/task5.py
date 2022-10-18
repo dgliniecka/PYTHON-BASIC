ffdfdaf32947598ed503aff2404977f3f6b0be95
Diana Gliniecka <107244877+dgliniecka@users.noreply.github.com>
2022-10-18 12:12:34 +0200

    Update task5.py

--- practice/1_python_part_1/task5.py
+++ practice/1_python_part_1/task5.py
@@ -10,7 +10,16 @@ Examples:
     >>> remove_duplicated_words('1 2 3')
     '1 2 3'
 """
-
-
 def remove_duplicated_words(line: str) -> str:
-    ...
+  result = ""
+  line = line.split()
+  line = sorted(set(line))
+  for l in line:
+    result = result + l + " "
+  print('Final string:', result)
+  result = result[:-1]
+  return result
+
+remove_duplicated_words('cat cat dog 1 dog 2')
+remove_duplicated_words('cat cat cat')
+remove_duplicated_words('1 2 3')
