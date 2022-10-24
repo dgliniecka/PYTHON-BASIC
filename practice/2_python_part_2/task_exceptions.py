8f6ac603aa0ada86053d123b155fd68a7925047a
dgliniecka <dgliniecka@griddynamics.com>
2022-10-24 15:02:54 +0200

    Not working yet

--- practice/2_python_part_2/task_exceptions.py
+++ practice/2_python_part_2/task_exceptions.py
@@ -4,18 +4,31 @@ If y == 0 it should print "Division by 0" and return None
 elif y == 1 it should raise custom Exception with "Deletion on 1 get the same result" text
 else it should return the result of division
 In all cases it should print "Division finished"
-    >>> division(1, 0)
+   division(1, 0)
     Division by 0
     Division finished
-    >>> division(1, 1)
+  division(1, 1)
     Division finished
     DivisionByOneException("Deletion on 1 get the same result")
-    >>> division(2, 2)
+   division(2, 2)
     1
     Division finished
 """
 import typing
-
-
+class DivisionByOneException: pass
 def division(x: int, y: int) -> typing.Union[None, int]:
-    ...
+
+    if y == 0:
+        print('Division by 0')
+        print('Division finished')
+        return None
+    elif y == 1:
+        Raise: DivisionByOneException('Deletion on 1 get the same result')
+        print('Division finished')
+    else:
+        result = x/y
+        print(result)
+        return result
+        print('Division finished')
+
+division(1, 1)
