24ebf709d2c799ff7607bd37a78808ae7b3c6829
dgliniecka <dgliniecka@griddynamics.com>
2022-10-25 10:52:10 +0200

    Works

--- practice/2_python_part_2/task_input_output.py
+++ practice/2_python_part_2/task_input_output.py
@@ -7,17 +7,44 @@ If average exists, return: "Avg: X", where X is avg value which rounded to 2 pla
 If it doesn't exists, return: "No numbers entered"
 Examples:
     user enters: 1, 2, hello, 2, world
-    >>> read_numbers(5)
+    read_numbers(5)
     Avg: 1.67
     ------------
     user enters: hello, world, foo, bar, baz
-    >>> read_numbers(5)
+   read_numbers(5)
     No numbers entered
 
 """
 
 
 def read_numbers(n: int) -> str:
-    ...
+    numbers = []
+    i = 0
+    while i < n:
+        i += 1
+        number = input('Write your number: ')
+        if number.isdigit():
+            integer = int(number)
+            numbers.append(integer)
+
+    if len(numbers) > 0:
+        result = sum(numbers)/len(numbers)
+        str_1 = "Avg"
+        print(str_1, ': ', result)
+        return str_1
+    else:
+        str_2 = 'No numbers entered'
+        print(str_2)
+        return str_2
+
+read_numbers(3)
+
+
+
+
+
+
+
+
 
 
