b0a4832265b71e26a3f980ee6919d354ef22265a
dgliniecka <dgliniecka@griddynamics.com>
2022-10-25 12:02:58 +0200

    another one bites the dust

--- practice/2_python_part_2/task_read_write.py
+++ practice/2_python_part_2/task_read_write.py
@@ -13,3 +13,23 @@ Example:
 
     result.txt(content: "23, 78, 3")
 """
+
+import csv
+def howmanyfiles(n):
+    n = int(input('Input how many files would you like to read: '))
+    i = 0
+    mystring = []
+
+    while i < n:
+        i += 1
+        filename = input('Gimme filename: ')
+        with open(filename) as opened_file:
+            infile = opened_file.read()
+            mystring.append(infile)
+
+    with open('result.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(mystring)
+    print(mystring)
+
+howmanyfiles(2)
